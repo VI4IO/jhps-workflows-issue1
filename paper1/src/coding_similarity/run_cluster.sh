49b4a53f23c25aa83bd04145d4bcb0a96341b408
Eugen Betke <eugen.betke@gmail.com>
2021-02-09 11:42:24 +0100

    Fix clustering script

--- paper1/src/coding_similarity/run_cluster.sh
+++ paper1/src/coding_similarity/run_cluster.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-label="small"
-dataset_fn="../../datasets/job_codings_v3_small.csv"
-output_fn="../../evaluation/job_codings_clusters_$label.csv"
-progress_fn="../../evaluation/progress_$label.csv"
+dataset_fn="../../datasets/job_codings.csv"
+output_fn="../../evaluation/job_codings_clusters.csv"
+progress_fn="../../evaluation/progress.csv"
 
 set -x
 rm $output_fn
